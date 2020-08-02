4652a022f790a0b5df05b193bb58ccad29e245c7
LDeolindo <lucas.deolindo1413@gmail.com>
2020-08-02 18:35:03 -0300

    Finished load queries

--- BancoDados/data base/loadData.py
+++ BancoDados/data base/loadData.py
@@ -23,16 +23,16 @@ def loadDataFromRelat(name, mycursor, path, table):
             INTO TABLE {} 
             CHARACTER SET Latin1 
             FIELDS TERMINATED BY ';' 
-            ENCLOSED BY '"' 
+            OPTIONALLY ENCLOSED BY '"' 
             LINES TERMINATED BY '\r\n' 
-            IGNORE 2 LINES 
+            IGNORE 3 LINES 
             (`Registro ANS`, CNPJ, `Razão Social`,
             `Nome Fantasia`, Modalidade, Logradouro,
             Número, Complemento, Bairro, Cidade,
             UF, CEP, DDD, Telefone, Fax,
             `Endereço eletrônico`, Representante,
             `Cargo Representante`, @`Data Registro ANS`)
-            SET `Data Registro ANS` = STR_TO_DATE(@`Data Registro ANS`, '%m/%d/%Y');
+            SET `Data Registro ANS` = STR_TO_DATE(@'`Data Registro ANS`', '%m/%d/%Y');
             """.format(path,table)
     mycursor.execute(query)
   except mysql.connector.Error as err:
@@ -45,29 +45,31 @@ def loadData(fileName, mycursor):
     path = '/var/lib/dados/{}'.format(name)
     table = name.split(".")[0]
     if name == 'Relatorio_cadop.csv': loadDataFromRelat(name, mycursor, path, table)
-    try:
-      print("Loading: {} - {}".format(path,table))
-      query = """LOAD DATA INFILE '{}' IGNORE
-              INTO TABLE {} 
-              CHARACTER SET Latin1 
-              FIELDS TERMINATED BY ';' 
-              ENCLOSED BY '"' 
-              LINES TERMINATED BY '\r\n' 
-              IGNORE 1 LINES 
-              (@DATA, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, @VL_SALDO_FINAL, `Registro ANS Relatorio`)
-              SET DATA = STR_TO_DATE(@DATA, '%m/%d/%Y'), 
-              VL_SALDO_FINAL = REPLACE(@VL_SALDO_FINAL,",",".");
-              """.format(path,table)
-      mycursor.execute(query)
-    except mysql.connector.Error as err:
-      print(err)
     else:
-      print("Loaded successfully!")
+      try:
+        print("Loading: {}".format(table))
+        query = """LOAD DATA INFILE '{}' IGNORE
+                INTO TABLE {} 
+                CHARACTER SET Latin1 
+                FIELDS TERMINATED BY ';' 
+                OPTIONALLY ENCLOSED BY '"' 
+                LINES TERMINATED BY '\r\n' 
+                IGNORE 1 LINES 
+                (@DATA, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, @VL_SALDO_FINAL)
+                SET DATA = STR_TO_DATE(@DATA, '%m/%d/%Y'),
+                VL_SALDO_FINAL = REPLACE(@"VL_SALDO_FINAL", ",", ".")
+                ;
+                """.format(path,table)
+        mycursor.execute(query)
+      except mysql.connector.Error as err:
+        print(err)
+      else:
+        print("Loaded successfully!")
 
 if __name__ == '__main__':
   fileName = os.listdir('./dados/.')
   mycursor = db.cursor()
   loadData(fileName, mycursor)
-  #db.commit()
+  db.commit()
   mycursor.close()
   db.close()
--- BancoDados/data base/queries.py
+++ BancoDados/data base/queries.py
@@ -1,9 +1,55 @@
-Montar uma query analítica que traga a resposta para as seguintes perguntas:
-  - Quais as 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre?
-  - Quais as 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último ano?
+# Montar uma query analítica que traga a resposta para as seguintes perguntas:
+#   - Quais as 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre?
+#   - Quais as 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último ano?
 
-searchByGoal = (status) => {
-    const goalsFiltered = this.state.goals.filter((item) => moment(item.year).format('YYYY') === this.state.yearSearch.format('YYYY') && item.goals.map(e => e.status).includes(status));
+# searchByGoal = (status) => {
+#     const goalsFiltered = this.state.goals.filter((item) => moment(item.year).format('YYYY') === this.state.yearSearch.format('YYYY') && item.goals.map(e => e.status).includes(status));
 
-    this.setState({ goalsFiltered, status });
-  }
+#     this.setState({ goalsFiltered, status });
+#   }
+
+# SELECT REG_ANS
+# FROM 4T2019
+# GROUP BY DESCRICAO = '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR' or 
+# 		DESCRICAO = '%EVENTOS/SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR' OR 
+#         DESCRICAO = '%EVENTOS / SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR' OR
+#         DESCRICAO = '%EVENTOS /SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR';
+
+import datetime
+import mysql.connector
+import tablesDictionary
+import config
+import createDataBase
+
+dbName = createDataBase.dbName
+
+try:
+  db = mysql.connector.connect(
+    host = config.host,
+    user = config.user,
+    password = config.password,
+    database = dbName
+  )
+except mysql.connector.Error as err:
+  print(err)
+
+def query(mycursor, text):
+  try:
+    print("Searching : ")
+    query = """
+            SELECT operadoras count(*)==10
+            FROM 1T2019, 2T2019, 3T2019, 4T2019, Relatorio_cadop
+            WHERE DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR';
+            """
+    mycursor.execute(query, (dateStart, dateEnd))
+  except mysql.connector.Error as err:
+    print(err)
+  else:
+    print("Loaded successfully!")
+
+if __name__ == '__main__':
+  mycursor = db.cursor()
+  text = "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR"
+  query(mycursor, text)
+  mycursor.close()
+  db.close()
