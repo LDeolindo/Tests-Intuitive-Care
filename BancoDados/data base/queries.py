c411ce85bd819d7e76cfcf1b2d97355b0a9b0a45
LDeolindo <lucas.deolindo1413@gmail.com>
2020-08-03 04:44:22 -0300

    Finished the third test

--- BancoDados/data base/queries.py
+++ BancoDados/data base/queries.py
@@ -1,20 +1,6 @@
 # Montar uma query analítica que traga a resposta para as seguintes perguntas:
 #   - Quais as 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre?
 #   - Quais as 10 operadoras que mais tiveram despesas com "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último ano?
-
-# searchByGoal = (status) => {
-#     const goalsFiltered = this.state.goals.filter((item) => moment(item.year).format('YYYY') === this.state.yearSearch.format('YYYY') && item.goals.map(e => e.status).includes(status));
-
-#     this.setState({ goalsFiltered, status });
-#   }
-
-# SELECT REG_ANS
-# FROM 4T2019
-# GROUP BY DESCRICAO = '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR' or 
-# 		DESCRICAO = '%EVENTOS/SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR' OR 
-#         DESCRICAO = '%EVENTOS / SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR' OR
-#         DESCRICAO = '%EVENTOS /SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR';
-
 import datetime
 import mysql.connector
 import tablesDictionary
@@ -33,23 +19,61 @@ try:
 except mysql.connector.Error as err:
   print(err)
 
-def query(mycursor, text):
+def queryQ4(mycursor):
   try:
     print("Searching : ")
     query = """
-            SELECT operadoras count(*)==10
-            FROM 1T2019, 2T2019, 3T2019, 4T2019, Relatorio_cadop
-            WHERE DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR';
+            SELECT `Razão Social`, `Registro ANS`
+            FROM Relatorio_cadop as REL, 
+              ( SELECT REG_ANS, DESCRICAO, SUM(VL_SALDO_FINAL) as SALDO
+                FROM 1T2020
+                WHERE DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR'
+                GROUP BY REG_ANS
+                ORDER BY SALDO DESC LIMIT 10
+              ) as DESP
+            WHERE REL.`Registro ANS` = DESP.REG_ANS;
             """
-    mycursor.execute(query, (dateStart, dateEnd))
+    mycursor.execute(query)
   except mysql.connector.Error as err:
     print(err)
   else:
-    print("Loaded successfully!")
+    print("As 10 operadoras que mais tiveram despesas  no último trimestre (1T - 2020):")
+    for (i, j) in mycursor:
+      print(" {} - {}".format(i, j))
+
+def queryPastYear(mycursor):
+  try:
+    print("Searching : ")
+    query = """
+            SELECT `Razão Social`, `Registro ANS`
+            FROM Relatorio_cadop as R, 
+              ( SELECT REG_ANS, DESCRICAO, SUM(VL_SALDO_FINAL) as SALDO
+                FROM (
+                      SELECT REG_ANS, DESCRICAO, VL_SALDO_FINAL FROM 1T2019
+                        UNION ALL
+                      SELECT REG_ANS, DESCRICAO, VL_SALDO_FINAL FROM 2T2019
+                        UNION ALL
+                      SELECT REG_ANS, DESCRICAO, VL_SALDO_FINAL FROM 3T2019
+                        UNION ALL
+                      SELECT REG_ANS, DESCRICAO, VL_SALDO_FINAL FROM 4T2019
+                      ) T
+                WHERE DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR'
+                GROUP BY REG_ANS
+                ORDER BY SALDO DESC LIMIT 10
+                ) as F
+            WHERE R.`Registro ANS` = F.REG_ANS;
+            """
+    mycursor.execute(query)
+  except mysql.connector.Error as err:
+    print(err)
+  else:
+    print("As 10 operadoras que mais tiveram despesas no último ano (2019):")
+    for (i, j) in mycursor:
+      print(" {} - {}".format(i, j))
 
 if __name__ == '__main__':
   mycursor = db.cursor()
-  text = "EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR"
-  query(mycursor, text)
+  queryQ4(mycursor)
+  queryPastYear(mycursor)
   mycursor.close()
   db.close()
